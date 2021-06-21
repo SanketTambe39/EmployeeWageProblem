d32d499c137905cb0abeddc140b456f9a850bb59
SanketTambe39 <sankettambe39@gmail.com>
2021-06-21 22:35:29 +0530

    Added: Part Time Details

--- EmployeWageProblem/src/com/employeewage/EmployeeWage.java
+++ EmployeWageProblem/src/com/employeewage/EmployeeWage.java
@@ -2,17 +2,31 @@ package com.employeewage;
 
 public class EmployeeWage {
 
+	public static final int IS_FULL_TIME=1;
+	public static final int IS_PART_TIME=2;
+	
 	public static void main(String[] args) {
-		int IS_PRESENT=1;
+
 		int WAGE_PER_HOUR=20;
-		int FULL_DAY_HOUR=8;
+		int EMP_HRs=0;
 		int DAILY_WAGE=0; 
 		
-		double empCheck=Math.floor(Math.random() * 10) % 2;
+		double empCheck=Math.floor(Math.random() * 10) % 3;
 
-			if(IS_PRESENT == empCheck) {
+		switch ((int) empCheck) {
+		case IS_FULL_TIME:
+				EMP_HRs=8;
+				break;
+		case IS_PART_TIME:
+				EMP_HRs=4;
+				break;
+		default:
+				EMP_HRs=0;
+		}
+		
+			if(empCheck>=1) {
 
-				DAILY_WAGE=WAGE_PER_HOUR*FULL_DAY_HOUR;
+				DAILY_WAGE=WAGE_PER_HOUR*EMP_HRs;
 				System.out.println("Employee is Present And His/Her Daily wage is :: "+DAILY_WAGE);
 			}
 			else
