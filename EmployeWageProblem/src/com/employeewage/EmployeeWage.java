94ef74ccd9a1a69e5df462357926f37d3a7d1e2b
SanketTambe39 <sankettambe39@gmail.com>
2021-06-21 22:24:08 +0530

    Updated: Employee Daily Wage

--- EmployeWageProblem/src/com/employeewage/EmployeeWage.java
+++ EmployeWageProblem/src/com/employeewage/EmployeeWage.java
@@ -4,16 +4,21 @@ public class EmployeeWage {
 
 	public static void main(String[] args) {
 		int IS_PRESENT=1;
-    	
-		double empCheck=Math.floor(Math.random() * 10) % 2;
+		int WAGE_PER_HOUR=20;
+		int FULL_DAY_HOUR=8;
+		int DAILY_WAGE=0; 
 		
-    	if(IS_PRESENT == empCheck) {
-			System.out.println("Employee is Present");
-		}
-		else
-		{
-			System.out.println("Employee is Absent");
-		}
+		double empCheck=Math.floor(Math.random() * 10) % 2;
+
+			if(IS_PRESENT == empCheck) {
+
+				DAILY_WAGE=WAGE_PER_HOUR*FULL_DAY_HOUR;
+				System.out.println("Employee is Present And His/Her Daily wage is :: "+DAILY_WAGE);
+			}
+			else
+			{
+				System.out.println("Employee is Absent");
+			}
 
 	}
 
